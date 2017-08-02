a9e2a9f25a0146a5581ecc618d7eb9a0afa6e2b5
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-08-02 11:01:50 +0200

    Initial Commit

---
+++ SmartFact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartFact
+//
+//  Created by Guillaume Loulier on 02/08/2017.
+//  Copyright © 2017 Guillaume Loulier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
