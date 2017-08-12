7255217cac29adca1dc3134ed95a98cc3ede8e06
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-08-12 18:55:39 +0200

    [ADD](Core)[!M] Backgroud images|Homepage -> Progress

---
+++ SmartFact/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  SmartFact
+//
+//  Created by Guillaume Loulier on 02/08/2017.
+//  Copyright © 2017 Guillaume Loulier. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    @IBOutlet weak var textOutlet: UITextField!
+    @IBAction func register() {
+        
+    }
+}
