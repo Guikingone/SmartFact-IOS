937cd9b1981774e6db817a14629a7ae0527fbaf3
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-09-06 19:36:32 +0200

    [ADD](Buttons + Tab bar)[!M] Buttons|Class created -> To configure + Tab Bar|Background updated

---
+++ SmartFact/View/BackBtn.swift
@@ -0,0 +1,14 @@
+//
+//  BackBtn.swift
+//  SmartFact
+//
+//  Created by Guillaume Loulier on 06/09/2017.
+//  Copyright © 2017 Guillaume Loulier. All rights reserved.
+//
+
+import UIKit
+
+class BackBtn: UIButton
+{
+
+}
